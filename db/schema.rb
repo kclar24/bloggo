d806f983e45fcd80df6799eb444497b70e4f4e9c
Keith Clark <geauxlsutigers24@gmail.com>
2016-10-31 15:01:53 +0000

    created scaffold for comment and migrated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161027153051) do
+ActiveRecord::Schema.define(version: 20161031145431) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "author"
+    t.text     "entry"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
