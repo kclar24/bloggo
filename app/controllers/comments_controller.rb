ccfce477920873a6745d8339c200115014b575b1
Keith Clark <geauxlsutigers24@gmail.com>
2016-11-01 13:39:34 +0000

    formatted to display comment section in post view

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -28,8 +28,8 @@ class CommentsController < ApplicationController
 
     respond_to do |format|
       if @comment.save
-        format.html { redirect_to @comment, notice: 'Comment was successfully created.' }
-        format.json { render :show, status: :created, location: @comment }
+         format.html { redirect_to post_path(id: @comment.post_id), notice: 'Comment was successfully created.' }
+         format.json { render :show, status: :created, location: @comment }
       else
         format.html { render :new }
         format.json { render json: @comment.errors, status: :unprocessable_entity }
