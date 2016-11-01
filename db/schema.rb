6b1c6a14be8a8bf8875881fd88553c60e1e1c795
Keith Clark <geauxlsutigers24@gmail.com>
2016-11-01 14:44:19 +0000

    created migration for userid to postand migrated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161101135848) do
+ActiveRecord::Schema.define(version: 20161101144233) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "author"
@@ -26,6 +26,8 @@ ActiveRecord::Schema.define(version: 20161101135848) do
     t.text     "entry"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id_id"
+    t.index ["user_id_id"], name: "index_posts_on_user_id_id"
   end
 
   create_table "users", force: :cascade do |t|
