efe3f599fe7da5b2bf255b4d515733c5bc63f9a3
Keith Clark <geauxlsutigers24@gmail.com>
2016-11-01 14:45:46 +0000

    created migration for userid to comments and migrated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161101144233) do
+ActiveRecord::Schema.define(version: 20161101144449) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "author"
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 20161101144233) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "post_id"
+    t.integer  "user_id_id"
+    t.index ["user_id_id"], name: "index_comments_on_user_id_id"
   end
 
   create_table "posts", force: :cascade do |t|
